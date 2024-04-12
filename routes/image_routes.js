c8aa8736dfab517c59cd1c20f08a0a877d5a4fce
Thomas Loo <tomloo222@gmail.com>
2024-04-12 12:05:24 +1000

    Change from presignedUrl to publicUrl for uploaded image; create randomised key for uploaded img

--- routes/image_routes.js
+++ routes/image_routes.js
@@ -5,6 +5,8 @@ import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } fro
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 import sharp from 'sharp'
 import dotenv from 'dotenv'
+import AWS from 'aws-sdk'
+import randomString from 'randomized-string'
 
 dotenv.config()
 const storage = multer.memoryStorage()
@@ -12,43 +14,43 @@ const upload = multer({ storage: storage })
 
 const router = express.Router()
 
-// Create new S3 client
-const s3 = new S3Client({
-    region: process.env.BUCKET_REGION,
-    credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY,
-        secretAccessKey: process.env.AWS_SACCESS_KEY,
-    },
+const s3 = new AWS.S3({
+    accessKeyId: process.env.AWS_ACCESS_KEY,
+    secretAccessKey: process.env.AWS_SACCESS_KEY
 })
 
 async function uploadToS3(file, updateId) {
 
+    // Check if file was sent with the POST request
     if (!file) {
         return { error: "No file"}
     }
 
+    // Resize image to A5 size, limit img size to send to S3
     let buffer = await sharp(file.buffer).resize({ height: 1169, width: 828, fit: "contain" }).toBuffer()
 
     const params = {
         Bucket: process.env.IMG_BUCKET,
-        Key: file.originalname,
+        // Randomise image key
+        Key: randomString.generate(),
         Body: buffer,
-        ContentType: file.mimetype,
+        ContentType: file.mimetype
     }
 
     try {
-        const command = new PutObjectCommand(params)
+        // Find the update in the database
         const update = await UpdateModel.findById(updateId)
+        // If image already exists, delete it
         if (update.img) {
-            await s3.send(new DeleteObjectCommand({ Bucket: process.env.IMG_BUCKET, Key: update.img }))
+            s3.deleteObject({ Bucket: process.env.IMG_BUCKET, Key: update.img })
         }
-        update.img = file.originalname
-        const url = await getSignedUrl(s3, new GetObjectCommand({ Bucket: process.env.IMG_BUCKET, Key: file.originalname }, { expiresIn: 604800 }))
-        update.imgUrl = url
+
+        // Upload new image
+        const url = await s3.upload(params).promise()
+        update.imgUrl = url.Location
         await update.save()
-        return await s3.send(command)
     } catch (err) {
-        console.log(err)
+        res.status(400).send({error: err})
     }
 }
 
